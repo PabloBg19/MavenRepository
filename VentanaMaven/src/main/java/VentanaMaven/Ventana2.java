22fc7f37158b4aa42de519f5eaacb9977506ef89
PabloBg19 <pbelascoainguerra@gmail.com>
2025-09-23 14:20:30 +0200

    sds

--- VentanaMaven/src/main/java/VentanaMaven/Ventana2.java
+++ VentanaMaven/src/main/java/VentanaMaven/Ventana2.java
@@ -3,20 +3,19 @@ package VentanaMaven;
 import javax.swing.*;
 
 public class Ventana2 {
-    private JPanel Panel2;
     private JTextField textField1;
-    private JPasswordField passwordField1;
-    private JRadioButton ACEPTARTERMINOSYCONDICIONESRadioButton;
     private JComboBox comboBox1;
-    private JLabel labelUsuario;
-    private JLabel labelContraseña;
-
+    private JRadioButton REMEMBERUSERRadioButton;
+    private JRadioButton ACCEPTTERMSANDCONDITIONSRadioButton;
+    private JButton ACCESSButton;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Ventana2");
-        frame.setContentPane(new Ventana2().Panel2);
+        frame.setContentPane(new Ventana2().PANEL);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(575, 567);
+        frame.setSize(550,450);
         frame.setVisible(true);
     }
+
+    private JPanel PANEL;
 }
